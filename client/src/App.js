6a72a16211bb159c7cd4d7f7b2bff24fcc962cfe
sonnezone <alex@gba.kz>
2023-06-13 18:51:54 +0200

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,3 @@
-// import React, { useState } from "react";
-//
-// import Main from "./components/main/main";
-// import Book from "./components/booking/book";
-// import ThankYou from "./components/booking/thankYou";
-// import Navbar from "./components/header/navbar";
-// import {Container} from "reactstrap";
-// import {BrowserRouter, Route, Router, Switch} from "react-router-dom";
-//
-// export default _ => {
-//   const [page, setPage] = useState(0);
-//
-//   return (
-//       <>
-//       <Switch>
-//               <Route path="/" component={<Container>
-//                   <Navbar setPage={setPage} />
-//                   {page === 0 ? <Main setPage={setPage} /> : null}
-//                   {page === 1 ? <Book setPage={setPage} /> : null}
-//                   {page === 2 ? <ThankYou /> : null}
-//               </Container>}/>
-//       </Switch>
-//           </>
-//   );
-// };
-
 import React from "react";
 import Main from "./components/main/main";
 import Book from "./components/booking/book";
@@ -48,4 +22,3 @@ export default () => {
         </Router>
     );
 };
-
